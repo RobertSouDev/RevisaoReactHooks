9cb6a303f69b5c8e186719146140f9d6f560a8b3
RobertSouDev <rogerhallks1@gmail.com>
2025-05-23 00:35:07 -0300

    refactor: rewrite counter component using TypeScript and Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import ClientList from "./components/ClientList"
+import Contador from "./components/Contador";
 
 function App() {
-
-  return (
-    
-    <div 
-      className="bg-gray-200 border-4 min-h-screen"
-    >
-      <ClientList></ClientList>
-    </div>
-    
-  )
+    return (
+        <div className="bg-gray-200 border-4 min-h-screen">
+            <Contador />
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/Contador.tsx
+++ src/components/Contador.tsx
@@ -1,20 +1,41 @@
 import { useState } from "react";
 
-export default  function Contador() {
-    const [number, setNumber] = useState(10)
+export default function Contador() {
+    const [number, setNumber] = useState<number>(10);
 
-    const Increase = () => setNumber(number => number + 1)
-    
-    const decrease = () => setNumber(number => number - 1)
+    const Increase = () => setNumber((number) => number + 1);
 
-    return ( 
-        <div className="w-90 h-90 border flex text-3xl justify-around items-center bg-gradient-to-br from-gray-900 via-purple-900 to-gray-800 p-8 flex justify-center">
+    const decrease = () => setNumber((number) => number - 1);
 
-            <button  className="border p-2 w-20 rounded-2xl" onClick={Increase}>+</button>
-            <h1>{number}</h1>
-            <button className="border p-2 w-20 rounded-2xl" onClick={decrease}>-</button>
+    const reset = () => setNumber(10);
+
+    return (
+        <div className="md:h-screen bg-gradient-to-br from-gray-900 via-purple-900 to-gray-800 p-8 flex justify-center items-center">
+            <div className="w-96 h-96 bg-gray-300 shadow-2xl rounded-4xl flex flex-col ">
+                <div className="h-72 text-3xl items-center flex justify-around">
+                    <button
+                        className="shadow-2xs p-2 w-20 rounded-2xl bg-green-400 text-white "
+                        onClick={Increase}
+                    >
+                        +
+                    </button>
+                    <h1>{number}</h1>
+                    <button
+                        className="shadow-2xs p-2 w-20 rounded-2xl bg-red-400 text-white "
+                        onClick={decrease}
+                    >
+                        -
+                    </button>
+                </div>
+                <div className="w-full h-20 flex justify-center items-center ">
+                    <button
+                        onClick={reset}
+                        className="flex items-center gap-2 bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-2xl transition-colors duration-300 text-2xl"
+                    >
+                        Reset
+                    </button>
+                </div>
+            </div>
         </div>
-     );
+    );
 }
-
-
